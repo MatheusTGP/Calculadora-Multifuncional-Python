eae18d0bb55f725a518ae60f1b149d4674ce5396
MatheusTGamerPro ツ <89700288+MatheusTGamerPro@users.noreply.github.com>
2021-08-29 01:19:02 -0300

    Atualização de Limpeza
    - Código Melhorado
    - Limpeza de linhas inúteis
    - Corrigir Espaços em Strings

--- Calculadora/Calculadora.py
+++ Calculadora/Calculadora.py
@@ -1,25 +1,18 @@
 import time
-print(' ======[ Menu_Inicial ]====== ')
-valor1 = int(input(" [ Valor1 ]: "))
+print('======[ Menu_Inicial ]======')
+valor1 = int(input("[ Valor1 ]: "))
 time.sleep(1)
-valor2 = int(input(" [ Valor2 ]: "))
+valor2 = int(input("[ Valor2 ]: "))
 time.sleep(1)
-print (" ")
-print (' Calculando Soma... ')
-print (" ")
-
+print(" ")
+print(' Calculando Soma... ')
+print(" ")
 # Calcular Resultados pela Máquina
-
 sun1 = valor1 + valor2
 sun2 = valor1 - valor2
 sun3 = valor1 * valor2
-
 # Executar Resultado
-
-print (' ====| Resultados |==== ')
-
-print (' {} + {} = [ {} ] \n {} - {} = [ {} ] \n {} × {} = [ {} ] \n '.format(valor1, valor2, sun1, valor1, valor2, sun2, valor1, valor2, sun3))
-
-print (' ====| Calculos |==== ')
-
+print('====| Resultados |====')
+print('{} + {} = [ {} ] \n {} - {} = [ {} ] \n {} × {} = [ {} ] \n '.format(valor1, valor2, sun1, valor1, valor2, sun2, valor1, valor2, sun3))
+print('====| Calculos |====')
 # Fim do Código, programa não executa automáticamente novamemte..
