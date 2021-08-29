89253f5351804db2a61ec583e087636cb3086083
MatheusTGamerPro ツ <89700288+MatheusTGamerPro@users.noreply.github.com>
2021-08-29 01:35:44 -0300

    Atualização ½
    - Adicionado o calculo: Divisão
    - Ajustamento do Temporizadores
    - Design do Código melhorado
    - Novos Textos

--- Calculadora/Calculadora.py
+++ Calculadora/Calculadora.py
@@ -1,18 +1,19 @@
 import time
 print('======[ Menu_Inicial ]======')
 valor1 = int(input("[ Valor1 ]: "))
-time.sleep(1)
+time.sleep(0)
 valor2 = int(input("[ Valor2 ]: "))
 time.sleep(1)
 print(" ")
-print(' Calculando Soma... ')
+print(' Calculando... ')
 print(" ")
 # Calcular Resultados pela Máquina
 sun1 = valor1 + valor2
 sun2 = valor1 - valor2
 sun3 = valor1 * valor2
+sun4 = valor1 / valor2
 # Executar Resultado
-print('====| Resultados |====')
-print('{} + {} = [ {} ] \n {} - {} = [ {} ] \n {} × {} = [ {} ] \n '.format(valor1, valor2, sun1, valor1, valor2, sun2, valor1, valor2, sun3))
+print('====| Resultados |==== \n')
+print(' [ {} + {} ] = [ {} ] \n [ {} - {} ] = [ {} ] \n [ {} × {} ] = [ {} ] \n [ {} ÷ {} ] = [ {} ] \n'.format(valor1, valor2, sun1, valor1, valor2, sun2, valor1, valor2, sun3, valor1, valor2, sun4))
 print('====| Calculos |====')
 # Fim do Código, programa não executa automáticamente novamemte..
