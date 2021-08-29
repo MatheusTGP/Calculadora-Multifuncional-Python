88408743e0f59df19422075a463b40348e536395
MatheusTGamerPro ツ <89700288+MatheusTGamerPro@users.noreply.github.com>
2021-08-29 10:29:55 -0300

    Atualização ½ 2
    - Alterar Design do Código
    - Remover Tiras de Espaços livres
    - adicionado outros textos!
    
    • Código Executado e Verificado!

--- Calculadora/Calculadora.py
+++ Calculadora/Calculadora.py
@@ -1,15 +1,14 @@
 # @Autor: MatheusTGamerPro
 # @GitHub: https://github.com//MatheusTGamerPro
 # @100% Python
-
 import time
-print('======[ Menu_Inicial ]======')
+print('======[ Calculadora Pré-Release]======')
 valor1 = int(input("[ Valor1 ]: "))
 time.sleep(0)
 valor2 = int(input("[ Valor2 ]: "))
 time.sleep(1)
 print(" ")
-print(' Calculando... ')
+print('Calculando...')
 print(" ")
 # Calcular Resultados pela Máquina
 sun1 = valor1 + valor2
@@ -17,7 +16,10 @@ sun2 = valor1 - valor2
 sun3 = valor1 * valor2
 sun4 = valor1 / valor2
 # Executar Resultado
-print('====| Resultados |==== \n')
-print(' [ {} + {} ] = [ {} ] \n [ {} - {} ] = [ {} ] \n [ {} × {} ] = [ {} ] \n [ {} ÷ {} ] = [ {} ] \n'.format(valor1, valor2, sun1, valor1, valor2, sun2, valor1, valor2, sun3, valor1, valor2, sun4))
-print('====| Calculos |====')
+print('====[ Calculo Efetuado ]==== \n')
+print('[ {} + {} ] = [ {} ] \n [ {} - {} ] = [ {} ] \n [ {} × {} ] = [ {} ] \n [ {} ÷ {} ] = [ {} ] \n'.format(valor1, valor2, sun1, valor1, valor2, sun2, valor1, valor2, sun3, valor1, valor2, sun4))
+print('\n Versão do código: Pré-Lançado')
+print('====[ Somas Verificadas ]====')
 # Fim do Código, programa não executa automáticamente novamemte..
+# Copyrigth © MatheusTGamerPro Code Original
+# Não delete as Linhas com HashTags!
