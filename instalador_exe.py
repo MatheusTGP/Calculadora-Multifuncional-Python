41951dc76609dfd77c0c9f0072cc5bca26a897ce
MatheusTGamerPro ツ <89700288+MatheusTGamerPro@users.noreply.github.com>
2021-09-20 15:34:47 -0300

    Criar Desenvolvedor Exe
    - Modificar Diretório do Arquivo EXE

---
+++ instalador_exe.py
@@ -0,0 +1,3 @@
+import shutil
+
+shutil.make_archive('Calculadora_V3,'MatheusTGamerPro/Calculadora-Multifuncional-Python/Slogan', 'D:\a\Calculadora-Multifuncional-Python\Calculadora-Multifuncional-Python\dist\Calculadora_2.0.exe')
